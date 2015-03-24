677a478b8950b65d1d95c2bda76eb73982b6cc0d
Andy Gocke <angocke@microsoft.com>
2015-03-24 00:18:08 -0700

    Add inputrc and add installation to install.bat

--- install.bat
+++ install.bat
@@ -2,8 +2,16 @@
 @echo off
 if not exist "%1" goto usage
 
+if exist "%1\.emacs" del "%1\.emacs"
+if exist "%1\.emacs.d" rmdir "%1\.emacs.d"
+if exist "%1\.inputrc" del "%1\.inputrc"
+
 mklink "%1\.emacs" "%~dp0emacs"
 mklink /D "%1\.emacs.d" "%~dp0emacs.d"
+
+mklink "%1\.inputrc" "%~dp0inputrc"
+
+goto exit
 
 :usage
 echo usage: install.bat home-dir
