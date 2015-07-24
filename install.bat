9580f495f7fd4e5dc5821b1f632af3f169f5b88f
Andy Gocke <angocke@microsoft.com>
2015-07-24 13:15:01 -0700

    Add bashrc and add bashrc install to install.bat

--- install.bat
+++ install.bat
@@ -5,11 +5,15 @@ if not exist "%1" goto usage
 if exist "%1\.vimrc" del "%1\.vimrc"
 if exist "%1\.vim" rmdir "%1\.vim"
 if exist "%1\.inputrc" del "%1\.inputrc"
+if exist "%1\.gitconfig" del "%1\.gitconfig"
+if exist "%1\.bashrc" del "%1\.bashrc"
 
 mklink "%1\.vimrc" "%~dp0vimrc"
 mklink /D "%1\.vim" "%~dp0vim"
 
 mklink "%1\.inputrc" "%~dp0inputrc"
+mklink "%1\.gitconfig" "%~dp0gitconfig"
+mklink "%1\.bashrc" "%~dp0bashrc"
 
 goto exit
 
