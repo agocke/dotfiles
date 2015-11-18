0cb95be1d2d934808898cb9b0334fe827546b2ba
Andy Gocke <angocke@microsoft.com>
2015-11-18 12:31:40 -0800

    Install vs-bash and bash_profile on Windows

--- install.bat
+++ install.bat
@@ -7,6 +7,7 @@ if exist "%1\.vim" rmdir "%1\.vim"
 if exist "%1\.inputrc" del "%1\.inputrc"
 if exist "%1\.gitconfig" del "%1\.gitconfig"
 if exist "%1\.bashrc" del "%1\.bashrc"
+if exist "%1\vs-bash.bat" del "%1\vs-bash.bat"
 
 mklink "%1\.vimrc" "%~dp0vimrc"
 mklink /D "%1\.vim" "%~dp0vim"
@@ -14,6 +15,7 @@ mklink /D "%1\.vim" "%~dp0vim"
 mklink "%1\.inputrc" "%~dp0inputrc"
 mklink "%1\.gitconfig" "%~dp0gitconfig"
 mklink "%1\.bashrc" "%~dp0bashrc"
+mklink "%1\vs-bash.bat" "%~dp0vs-bash.bat"
 
 goto exit
 
---
+++ vs-bash.bat
@@ -0,0 +1,4 @@
+@echo off
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Git\bin\bash.exe" --login -i
