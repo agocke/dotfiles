44fe4a0d0bb521dd48ebb44efd3a589e455608cb
Andy Gocke <angocke@microsoft.com>
2015-11-18 12:45:50 -0800

    Update vs-bash for Windows Git 2.x

--- vs-bash.bat
+++ vs-bash.bat
@@ -1,4 +1,4 @@
 @echo off
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-call "%ProgramFiles(x86)%\Git\bin\bash.exe" --login -i
+call "%LocalAppData%\Programs\Git\usr\bin\bash.exe" --login -i
