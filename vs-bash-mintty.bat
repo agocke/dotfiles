14c78c7edec26420e3a7601d38391b98dab4de99
Andy Gocke <angocke@microsoft.com>
2017-02-15 15:32:55 -0800

    Update VS Bash links to use VS 2017

--- vs-bash-mintty.bat
+++ vs-bash-mintty.bat
@@ -1,4 +1,4 @@
 @echo off
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 start "" "C:\Program Files\Git\git-bash.exe" --cd-to-home
--- vs-bash.bat
+++ vs-bash.bat
@@ -1,4 +1,4 @@
 @echo off
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 call "%ProgramFiles%\Git\usr\bin\bash.exe" --login -i
