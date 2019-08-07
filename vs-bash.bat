69ecdc42ebedb6a91a2190b2c3f0f945dc4a974d
Andy Gocke <angocke@microsoft.com>
2019-08-07 11:24:18 -0700

    Update VS bash files

--- vs-bash.bat
+++ vs-bash.bat
@@ -1,4 +1,4 @@
 @echo off
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\Preview\Enterprise\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\Common7\Tools\VsDevCmd.bat"
 "C:\Program Files\Git\bin\bash.exe" --login
