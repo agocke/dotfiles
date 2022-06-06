ffe79da56c46657358ec0d1ae7499160097c50bd
Andy Gocke <angocke@microsoft.com>
2022-06-06 14:01:03 -0700

    Use VS 2022 link

--- vs-bash.bat
+++ vs-bash.bat
@@ -1,4 +1,4 @@
 @echo off
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat"
 "C:\Program Files\Git\bin\bash.exe" --login
